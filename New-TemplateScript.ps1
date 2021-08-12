42f54221bb4f2ccef252ce47aaef894cec8e5041
Andrew Zbikowski <azbikowski@ramquest.com>
2021-08-12 11:30:39 -0500

    Bad comma

--- New-TemplateScript.ps1
+++ New-TemplateScript.ps1
@@ -9,7 +9,7 @@ Function New-TemplateScript {
 		[string]$NewScriptName,
 		[Parameter(Mandatory=$false,
 		Position=1)]
-		[string]$TemplateURI="https://raw.githubusercontent.com/andyzib/Templates/master/PowerShell_ScriptTemplate.ps1",		
+		[string]$TemplateURI="https://raw.githubusercontent.com/andyzib/Templates/master/PowerShell_ScriptTemplate.ps1"	
 	)	  
 	# Strip illegal file name characters with a RegEx.
 	# https://gallery.technet.microsoft.com/scriptcenter/Remove-Invalid-Characters-39fa17b1
